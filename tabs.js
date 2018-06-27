0fb1f056ad1ecaaa85a9e584b7d16c7795f62066
dandelia <pugongyingjin@hotmail.com>
2018-06-27 17:00:04 +0800

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,16 @@
+function questions(event, question) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(question).style.display = "block";
+    event.currentTarget.className += " active";
+}
